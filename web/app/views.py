f2a4d7a38e6e926204483e0f013cc42d2076cd96
Koh Wei Jie <contact@kohweijie.com>
2018-02-08 23:12:45 +0800

    debugging django

--- web/app/views.py
+++ web/app/views.py
@@ -9,18 +9,16 @@ from project import settings
 @login_required
 def index(request):
     lang = request.META.get('LANGUAGE', request.META.get('LANG'))
-    print(lang)
+
     if lang is None:
         lang = "en"
     elif lang == "zh":
         pass
-    else:
+    elif ":" in lang:
         try:
             lang = lang.split(":")[1]
         except:
             lang = "en"
-
-    print(lang)
 
     return render(
             request,
