b48e96909da0b891d5cd7a07bdf6b9ccc35311cf
Koh Wei Jie <contact@kohweijie.com>
2018-02-08 22:58:05 +0800

    debugging django

--- web/project/settings.py
+++ web/project/settings.py
@@ -29,7 +29,8 @@ if "DJ_SECRET_KEY" in os.environ:
 LOGIN_REDIRECT_URL = '/'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = True
 
 if "DEV" in os.environ:
     DEBUG = os.environ["DEV"].lower() == "true"
@@ -67,10 +68,10 @@ HTML_MINIFY = True
 
 CSRF_TRUSTED_ORIGINS = [
     'cs.kohweijie.com',
-    'd36gysogqvy4e7.cloudfront.net',
+    # 'd36gysogqvy4e7.cloudfront.net',
 
-    'smarthongbao.com',
-    'd2jfrgsjvyi0al.cloudfront.net',
+    # 'smarthongbao.com',
+    # 'd2jfrgsjvyi0al.cloudfront.net',
 ]
 
 ROOT_URLCONF = 'project.urls'
