7e226176d15e0d78cb2b4464ad08a4ec95b0fdee
Koh Wei Jie <contact@kohweijie.com>
2018-02-09 18:10:45 +0800

    redesign done

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -16,18 +16,21 @@ gulp.task('static', shell.task([
 
 // Run preact build in production mode, and delete JS sourcemaps
 gulp.task('build-prod', shell.task([
-  'cd app/frontend && nwb build --no-vendor --no-html --config nwb.config.js',
-  'rm -rf ./app/frontend/dist/*.map',
-  'rm -rf ./project/static/',
-  'mkdir -p ./project/static/',
-  'rm -rf ./project/static/index.html',
-  'mv ./app/frontend/dist/* ./project/static/',
-  'rm -rf ./app/frontend/dist/',
-  'mv ./project/static/app.*.js ./project/static/app.js',
-  'mkdir -p ./app/frontend/images ./app/frontend/favicons',
-  'cp -r ./app/frontend/images ./project/static/images',
-  'cp -r ./app/frontend/favicons ./project/static/favicons',
-  'cp -r ./app/frontend/fonts ./project/static/fonts',
+    'cd app/frontend && nwb build --no-vendor --no-html --config nwb.config.js',
+    'echo "Deleting sourcemaps..."',
+    'rm -rf ./app/frontend/dist/*.map',
+    'echo "Renaming output files..."',
+    'rm -rf ./project/static/',
+    'mkdir -p ./project/static/',
+    'rm -rf ./project/static/index.html',
+    'mv ./app/frontend/dist/* ./project/static/',
+    'rm -rf ./app/frontend/dist/',
+    'mv ./project/static/app.*.js ./project/static/app.js',
+    'echo "Copying images..."',
+    'mkdir -p ./app/frontend/images ./app/frontend/favicons',
+    'cp -r ./app/frontend/images ./project/static/images',
+    'cp -r ./app/frontend/favicons ./project/static/favicons',
+    'cp -r ./app/frontend/fonts ./project/static/fonts',
 ],
   {
     env: {
