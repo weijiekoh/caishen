b6336c26d012b57899716a86148630d84c60f4e8
Koh Wei Jie <contact@kohweijie.com>
2018-02-09 23:08:26 +0800

    redeem date bugfix

--- web/app/frontend/src/dates.js
+++ web/app/frontend/src/dates.js
@@ -1,20 +1,20 @@
 const formatDate = (timestamp, isZh) => {
+  const months = ["January", "February", "March", 
+                  "April", "May", "June",
+                  "July", "August", "September",
+                  "October", "November", "December"];
+
   const d = new Date(timestamp.getTime() * 1000);
   const year = d.getFullYear();
   const month = months[d.getMonth()];
   const day = d.getDate();
+
   if (isZh){
     return year.toString() + "年" +
           (d.getMonth() + 1).toString() + "月" + 
           day.toString() + "日";
   }
   else{
-    const months = ["January", "February", "March", 
-                    "April", "May", "June",
-                    "July", "August", "September",
-                    "October", "November", "December"];
-
-
     return day.toString() + " " + month + " " + year.toString();
   }
 }
