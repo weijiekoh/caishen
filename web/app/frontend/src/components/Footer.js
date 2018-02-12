e5511720ce2ea61b72ec2b83c31bce2703037e89
Koh Wei Jie <contact@kohweijie.com>
2018-02-12 18:11:01 +0800

    footer

--- web/app/frontend/src/components/Footer.js
+++ web/app/frontend/src/components/Footer.js
@@ -4,13 +4,25 @@ export default class Home extends Component{
   render() {
     return(
       <div class="footer">
-        <p>
-          questions@smarthongbao.com
-        </p>
         {this.props.isZh ?
-          <p>© 财神{new Date().getFullYear()}</p>
+          <p>
+            支持我们， 商务合作，媒体报道 - <a 
+              target="_blank"
+              href="mailto:questions@smarthongbao.com">questions@smarthongbao.com</a>
+            <br />
+            <a target="_blank" href="https://github.com/weijiekoh/caishen">
+              源代码</a> | <a target="_blank" href="https://smarthongbao.us17.list-manage.com/subscribe?u=42144641b785b567481776096&id=b9a0fe1fe5">订阅邮件</a>
+          </p>
           :
-          <p>© CaiShen {new Date().getFullYear()}</p>
+          <p>
+            Support, business, and media enquiries - <a 
+              target="_blank"
+              href="mailto:questions@smarthongbao.com">questions@smarthongbao.com</a>
+            <br />
+            <a target="_blank" href="https://github.com/weijiekoh/caishen">
+              Source code</a> | <a target="_blank" href="https://smarthongbao.us17.list-manage.com/subscribe?u=42144641b785b567481776096&id=b9a0fe1fe5">Subscribe to our mailing list</a>
+
+          </p>
         }
       </div>
     )
